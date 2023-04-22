140796b492bba40d1955d3e5f6dd46f1849269fc
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-22 20:23:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+from stmol import showmol
+import py3Dmol
+import requests
+import biotite.structure.io as bsio
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+# stmol
+def render_mol(pdb):
+    pdbview = py3Dmol.view()
+    pdbview.addModel(pdb,'pdb')
+    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
+    pdbview.setBackgroundColor('white')#('0xeeeeee')
+    pdbview.zoomTo()
+    pdbview.zoom(2, 800)
+    pdbview.spin(True)
+    showmol(pdbview, height = 500,width=800)
+    
+txt = st.sidebar.text_area('Input sequence', DEFAULT_SEQ, height=275)

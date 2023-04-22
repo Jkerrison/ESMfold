dae53a13dff5141dd4bbf3e8e26ca74b889a6420
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-22 20:57:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,12 @@ def update(sequence=txt):
         mime='text/plain',
     )
 
+    
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
 
 
 predict = st.sidebar.button('Predict', on_click=update)

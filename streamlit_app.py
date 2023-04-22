bdee504bacc90ba689cec7823acd9207f95e452e
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-22 20:24:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ import requests
 import biotite.structure.io as bsio
 
 
+st.sidebar.title('ESMFold')
+# st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
+
+
 # stmol
 def render_mol(pdb):
     pdbview = py3Dmol.view()

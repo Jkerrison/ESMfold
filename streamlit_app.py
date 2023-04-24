6581d462544f5af75e87bc8f6d5081c0d7f9d32e
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-24 02:21:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end singl
 def render_mol(pdb):
     pdbview = py3Dmol.view()
     pdbview.addModel(pdb,'pdb')
-    pdbview.setStyle({'cartoon':{'color':'bfactor'}})
+    pdbview.setStyle({'cartoon':{'color':'chain'}})
     pdbview.setBackgroundColor('white')#('0xeeeeee')
     pdbview.zoomTo()
     pdbview.zoom(2, 800)

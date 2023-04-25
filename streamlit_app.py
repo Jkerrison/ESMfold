ad479637a574b640d96b399eef0c222345860852
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-25 11:27:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,12 +72,12 @@ for uploaded_file in uploaded_files:
         file_name=f'{uploaded_file.name[:-3]}.pdb',
         mime='text/plain',
     )
-    st.write(uploaded_file.name[:-3])
+    
     # save as third value of dictionary, add each item to a zip file for download
     struct = bsio.load_structure('predicted.pdb', extra_fields=["b_factor"])
     #st.write(pdb_string)
     b_value = round(struct.b_factor.mean(), 4)
-    
+    st.write(struct.b_factor.all())
     # Display protein structure
     st.subheader(f'Visualization of {uploaded_file.name[:-3]}')
     render_mol(pdb_string)

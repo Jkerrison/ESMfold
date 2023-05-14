4f788adb96a522876cdfecb2beca1ee08f95c90e
Jkerrison <jk1185@york.ac.uk>
2023-05-14 22:59:41 +0100

    back to original
    asd
    :q
    clear

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,60 +26,60 @@ def render_mol(pdb):
 # Protein sequence input 
 DEFAULT_SEQ = ""
 txt = st.sidebar.text_area('Input sequence', DEFAULT_SEQ, height=275)
-line_data=''
+# line_data=''
 
 
 
-#Directory
-directory = 'my_folder'
-if not os.path.exists(directory):
-    os.makedirs(directory)
+# #Directory
+# directory = 'my_folder'
+# if not os.path.exists(directory):
+#     os.makedirs(directory)
 
-uploaded_files = st.sidebar.file_uploader("Upload Fasta files", accept_multiple_files=True)
-list_of_files={"Name":[],"Sequence":[]}
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    str_data = bytes_data.decode('utf-8')  # convert bytes to str
-    line_data = str_data.split('\n')[1]
-    #st.write("filename:", uploaded_file.name)
-    #st.write(bytes_data)
-    list_of_files["Name"].append(uploaded_file.name[:-3])
-    list_of_files["Sequence"].append(line_data)
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/', headers=headers, data=line_data)
-    name = line_data[:3] + line_data[-3:]
-    pdb_string = response.content.decode('utf-8')
+# uploaded_files = st.sidebar.file_uploader("Upload Fasta files", accept_multiple_files=True)
+# list_of_files={"Name":[],"Sequence":[]}
+# for uploaded_file in uploaded_files:
+#     bytes_data = uploaded_file.read()
+#     str_data = bytes_data.decode('utf-8')  # convert bytes to str
+#     line_data = str_data.split('\n')[1]
+#     #st.write("filename:", uploaded_file.name)
+#     #st.write(bytes_data)
+#     list_of_files["Name"].append(uploaded_file.name[:-3])
+#     list_of_files["Sequence"].append(line_data)
+#     headers = {
+#         'Content-Type': 'application/x-www-form-urlencoded',
+#     }
+#     response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/', headers=headers, data=line_data)
+#     name = line_data[:3] + line_data[-3:]
+#     pdb_string = response.content.decode('utf-8')
 
-    with open(f'{directory}/{uploaded_file.name[:-3]}.pdb', 'w') as f:
-        f.write(pdb_string)
-    st.download_button(
-        label=f"Download {uploaded_file.name[:-3]}",
-        data=pdb_string,
-        file_name=f'{uploaded_file.name[:-3]}.pdb',
-        mime='text/plain',
-    )
-    # save as third value of dictionary, add each item to a zip file for download
-    struct = bsio.load_structure('predicted.pdb', extra_fields=["b_factor"])
-    b_value = round(struct.b_factor.mean(), 4)
+#     with open(f'{directory}/{uploaded_file.name[:-3]}.pdb', 'w') as f:
+#         f.write(pdb_string)
+#     st.download_button(
+#         label=f"Download {uploaded_file.name[:-3]}",
+#         data=pdb_string,
+#         file_name=f'{uploaded_file.name[:-3]}.pdb',
+#         mime='text/plain',
+#     )
+#     # save as third value of dictionary, add each item to a zip file for download
+#     struct = bsio.load_structure('predicted.pdb', extra_fields=["b_factor"])
+#     b_value = round(struct.b_factor.mean(), 4)
 
-    # Display protein structure
-    st.subheader(f'Visualization of {uploaded_file.name[:-3]}')
-    render_mol(pdb_string)
+#     # Display protein structure
+#     st.subheader(f'Visualization of {uploaded_file.name[:-3]}')
+#     render_mol(pdb_string)
     
-#st.write(list_of_files)
+# #st.write(list_of_files)
 
-with ZipFile(f'{directory}.zip', 'w') as zip:
-    for file in os.listdir(directory):
-        zip.write(os.path.join(directory, file), file)
+# with ZipFile(f'{directory}.zip', 'w') as zip:
+#     for file in os.listdir(directory):
+#         zip.write(os.path.join(directory, file), file)
 
-# create a download button for the zip file
-st.download_button(
-    label="Download all files as zip",
-    data=open(f'{directory}.zip', 'rb').read(),
-    file_name=f'{directory}.zip'
-)
+# # create a download button for the zip file
+# st.download_button(
+#     label="Download all files as zip",
+#     data=open(f'{directory}.zip', 'rb').read(),
+#     file_name=f'{directory}.zip'
+# )
 
 
     
